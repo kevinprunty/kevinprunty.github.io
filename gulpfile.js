681301ad23d01db4bb046ab92a50b4604a25333f
Kevin Prunty <kevin.prunty95@gmail.com>
2017-12-08 10:56:07 -0500

    Added uglify and concat tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,39 @@
 'use strict';
-const gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    lazypipe = require('lazypipe'),
-    rename = require("gulp-rename");
+//Required modules for gulp
+const   gulp = require('gulp'),
+        g_concat = require('gulp-concat'),
+        g_uglify = require('gulp-uglify'),
+        lazypipe = require('lazypipe'),
+        rename = require("gulp-rename");
+
+//Constant variables
+const scriptsFolder = 'scripts';
+
+
 
 const uglify = lazypipe()
-              .pipe(uglify())
-              .pipe(dest('scripts'));
+              .pipe(g_uglify)
+              .pipe(rename, {
+                dirname: "./",
+                basename: "app",
+                suffix: ".min", 
+                extname: ".js"
+            })
+              .pipe(gulp.dest, scriptsFolder);
 
 const concat = lazypipe()
-              .pipe(concat())
+              .pipe(g_concat)
+              .pipe(gulp.dest, scriptsFolder);
+
+//Uglify app.js file
+gulp.task('uglify', function(){
+        gulp.src('scripts/app.js')
+        .pipe(uglify());
+    
+});
+
+gulp.task('concat', function(){
+    return gulp.src('scripts/sources/*')
+            .pipe(concat)
+            .pipe(gulp.dest, scriptsFolder/app.js);
+});
